b63053e1f524f9426a6526fa32ae9eb6b4a4d8e6
Denisa_13 <56766058+denis156@users.noreply.github.com>
2025-06-17 04:51:01 +0800

    Add DashboardServiceProvider to the application service providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthRateLimiterServiceProvider::class,
+    App\Providers\DashboardServiceProvider::class,
     App\Providers\InspiringServiceProvider::class,
 ];
