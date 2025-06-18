ac5464775e8a329c8a7af3956901c6d2713fae82
Denisa_13 <56766058+denis156@users.noreply.github.com>
2025-06-18 22:33:55 +0800

    refactor: Remove unused authentication middleware and rate limiter service provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,7 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\AuthRateLimiterServiceProvider::class,
     App\Providers\DashboardServiceProvider::class,
     App\Providers\GeolocationServiceProvider::class,
     App\Providers\InspiringServiceProvider::class,
