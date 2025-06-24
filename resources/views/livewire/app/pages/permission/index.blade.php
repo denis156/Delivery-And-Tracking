83be102cad769c97f4d3819289a6b2496f32d948
Denisa_13 <56766058+denis156@users.noreply.github.com>
2025-06-25 07:23:59 +0800

    fix: Remove unnecessary grid column for permission and user cards to improve layout consistency

--- resources/views/livewire/app/pages/permission/index.blade.php
+++ resources/views/livewire/app/pages/permission/index.blade.php
@@ -99,7 +99,7 @@
         {{-- PERMISSION CARDS --}}
         <x-card class="p-6">
             {{-- Grid Layout menggunakan DaisyUI responsive grid --}}
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
                 @foreach($permissions as $permission)
                     @php
                         $category = $this->getPermissionCategory($permission->name);
--- resources/views/livewire/app/pages/user/index.blade.php
+++ resources/views/livewire/app/pages/user/index.blade.php
@@ -99,7 +99,7 @@
         {{-- USER CARDS --}}
         <x-card class="p-6">
             {{-- Grid Layout menggunakan DaisyUI responsive grid --}}
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
                 @foreach($users as $user)
                     {{-- User Card menggunakan Model Accessors --}}
                     <x-card
